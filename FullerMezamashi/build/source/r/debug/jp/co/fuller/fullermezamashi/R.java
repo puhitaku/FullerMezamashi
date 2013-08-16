1bf4ddfc736ee7955ac13eebf88c194b23e6f8b9
Takumi Sueda <puhitaku@gmail.com>
2013-08-16 11:22:51 +0900

    Event listener

--- FullerMezamashi/build/source/r/debug/jp/co/fuller/fullermezamashi/R.java
+++ FullerMezamashi/build/source/r/debug/jp/co/fuller/fullermezamashi/R.java
@@ -21,7 +21,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int btnSet=0x7f080002;
+        public static final int textView=0x7f080000;
+        public static final int timePicker=0x7f080001;
     }
     public static final class layout {
         public static final int activity_setting=0x7f030000;
@@ -34,7 +37,9 @@ public final class R {
          */
         public static final int action_settings=0x7f050000;
         public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int choose=0x7f050002;
+        public static final int hello_world=0x7f050003;
+        public static final int set_time=0x7f050004;
     }
     public static final class style {
         /**  From: file:/home/puhitaku/Development/android/F_Mezamashi/FullerMezamashi/src/main/res/values/styles.xml 
--- FullerMezamashi/src/main/java/jp/co/fuller/fullermezamashi/SettingActivity.java
+++ FullerMezamashi/src/main/java/jp/co/fuller/fullermezamashi/SettingActivity.java
@@ -3,7 +3,9 @@ package jp.co.fuller.fullermezamashi;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SettingActivity extends Activity {
 
@@ -12,7 +14,13 @@ public class SettingActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        Button btnSet = (Button)findViewById(R.id.)
+        Button btnSet = (Button)findViewById(R.id.btnSet);
+        btnSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SettingActivity.this, "Whoa!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
