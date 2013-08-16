fc952e779a5d7bd8815b332ae1e0475f159e1ba1
Takumi Sueda <puhitaku@gmail.com>
2013-08-16 13:03:39 +0900

    asdfg

--- FullerMezamashi/src/main/java/jp/co/fuller/fullermezamashi/SettingActivity.java
+++ FullerMezamashi/src/main/java/jp/co/fuller/fullermezamashi/SettingActivity.java
@@ -1,10 +1,15 @@
 package jp.co.fuller.fullermezamashi;
 
 import android.app.Activity;
+import android.app.KeyguardManager;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.Vibrator;
+import android.text.format.Time;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class SettingActivity extends Activity {
@@ -18,12 +23,43 @@ public class SettingActivity extends Activity {
         btnSet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                TimePicker pkrTime = (TimePicker)findViewById(R.id.timePicker);
+                wakeSoon( pkrTime );
                 Toast.makeText(SettingActivity.this, "Whoa!", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    public void wakeSoon(TimePicker picker) {
+        Integer hrs = picker.getCurrentHour();
+        Integer min = picker.getCurrentMinute();
+        PowerManager.WakeLock wakelock;
+        KeyguardManager keyguard;
+        KeyguardManager.KeyguardLock keylock;
+        Vibrator vib = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 
+        Toast.makeText(SettingActivity.this, "アラームは" + hrs.toString() + "時" + min.toString() + "分" + "にセットされています",
+                    Toast.LENGTH_SHORT).show();
+
+        Time time = new Time("Asia/Tokyo");
+        time.setToNow();
+        //while(time.hour != hrs && time.minute != min) {
+        //}
+
+        vib.vibrate(100);
+
+        /*
+        wakelock = ((PowerManager) getSystemService(Context.POWER_SERVICE))
+                .newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK
+                                |PowerManager.ACQUIRE_CAUSES_WAKEUP
+                                |PowerManager.ON_AFTER_RELEASE, "disableLock");
+        wakelock.acquire();
+
+        keyguard = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+        keylock = keyguard.newKeyguardLock("disableLock");
+        keylock.disableKeyguard();
+        */
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
